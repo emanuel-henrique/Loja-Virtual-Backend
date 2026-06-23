40adb4dfb37d401f9f57832c9c3fad4c45bf2e23
Emanuel Henrique <emanuelhenrique1562@gmail.com>
2026-06-23 18:52:44 -0300

    Update couponController.ts

--- src/controllers/couponController.ts
+++ src/controllers/couponController.ts
@@ -5,7 +5,6 @@ import { asyncHandler } from '../middlewares/errorHandler';
 const couponService = new CouponService();
 
 export const validateCoupon = asyncHandler(async (req: Request, res: Response) => {
-  // Garantindo que 'code' seja lido estritamente como uma string vinda do body
   const { code } = req.body as { code: string };
   const coupon = await couponService.validateCoupon(code);
   res.json({
@@ -20,8 +19,7 @@ export const createCoupon = asyncHandler(async (req: Request, res: Response) => 
 });
 
 export const deleteCoupon = asyncHandler(async (req: Request, res: Response) => {
-  // Forçando o parâmetro a ser tratado estritamente como string para eliminar o erro TS2345
-  const { id } = req.params as { id: string };
+  const id = req.params.id as string;
   await couponService.deleteCoupon(id);
   res.status(204).send();
 });
